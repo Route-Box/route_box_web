eb3ca523ce89395587ae167bbf0d424719be7d58
devGyulMem <khmdevmem@gmail.com>
2024-07-12 21:43:50 +0900

    Chore: eslint, prettier, 절대경로, 테스트코드 설정

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,6 +1,6 @@
-import { expect, test } from "vitest";
-import { sum } from "./sum";
+import { expect, test } from 'vitest';
+import { sum } from './sum';
 
-test("1 더하기 2는 3이다.", () => {
+test('1 더하기 2는 3이다.', () => {
   expect(sum(1, 2)).toBe(3);
 });
