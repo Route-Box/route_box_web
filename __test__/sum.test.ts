5ff18124d0abd68c369ff4f6410ebc19caebc4e4
devGyulMem <khmdevmem@gmail.com>
2024-07-19 23:17:36 +0900

    Test: sample test code

--- __test__/sum.test.ts
+++ __test__/sum.test.ts
@@ -4,3 +4,11 @@ import { sum } from '../src/sum';
 test('add 1+ 2 is 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('a값의 상한은 100이다.', () => {
+  expect(sum(90, 70)).toBe(160);
+});
+
+test('a값이 100 이상일 경우 에러가 발생한다.', () => {
+  expect(() => sum(110, 20)).toThrow('0~100 사이의 값을 입력해주세요.');
+});
--- src/sum.ts
+++ src/sum.ts
@@ -1,3 +1,7 @@
 export function sum(a: number, b: number) {
+  if (a < 0 || a > 100) {
+    throw new Error('0~100 사이의 값을 입력해주세요.');
+  }
+
   return a + b;
 }
