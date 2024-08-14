6bf121c2d37b454ae243aaff7d38aae737d7a57b
imi21123 <imi21123@naver.com>
2024-08-15 01:12:56 +0900

    Feat: 설정-회원 정보 수정 성별 퍼블리싱

---
+++ src/components/setting/profile/info-input/gender/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { GenderContainer, GenderSelect, GenderType } from './style';
+import { InputLabel } from '../style';
+
+export const Gender: React.FC = () => {
+  const [selectedGender, setSelectedGender] = useState<string | null>(null);
+
+  const handleSelect = (gender: string) => {
+    setSelectedGender(gender);
+  };
+
+  return (
+    <GenderContainer>
+      <InputLabel>성별</InputLabel>
+      <GenderType>
+        <GenderSelect isSelected={selectedGender === '남성'} onClick={() => handleSelect('남성')}>
+          남성
+        </GenderSelect>
+        <GenderSelect isSelected={selectedGender === '여성'} onClick={() => handleSelect('여성')}>
+          여성
+        </GenderSelect>
+        <GenderSelect
+          isSelected={selectedGender === '비공개'}
+          onClick={() => handleSelect('비공개')}
+        >
+          비공개
+        </GenderSelect>
+      </GenderType>
+    </GenderContainer>
+  );
+};
