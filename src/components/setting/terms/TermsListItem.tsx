eacaae68a377a928f89daa6a04dc68e24ff5948d
devGyulMem <khmdevmem@gmail.com>
2024-09-22 17:54:37 +0900

    Feat: external url 연결

--- src/components/setting/terms/TermsListItem.tsx
+++ src/components/setting/terms/TermsListItem.tsx
@@ -5,11 +5,12 @@ import { Item } from './style';
 
 interface TermsListProps {
   term: string;
+  url: string;
 }
 
-const TermsListItem: React.FC<TermsListProps> = ({ term }) => {
+const TermsListItem: React.FC<TermsListProps> = ({ term, url }) => {
   return (
-    <Item className="body-b-m">
+    <Item className="body-b-m" onClick={() => window.open(url, '_blank')}>
       {term}
       <button>
         <img src={ArrowRight} />
--- src/components/setting/terms/index.tsx
+++ src/components/setting/terms/index.tsx
@@ -4,11 +4,15 @@ import { Container } from './style';
 
 const TermsList: React.FC = () => {
   const terms = ['이용약관', '개인정보처리방침', '위치정보이용 약관'];
-
+  const urls = [
+    'https://www.notion.so/10845c063330803d8e96e6d58739a77b',
+    'https://www.notion.so/10845c063330800488e9e377850fe8e0',
+    'https://www.notion.so/f9b6a0a31d8b4334900137aa9cb023f3',
+  ];
   return (
     <Container>
       {terms.map((term, index) => (
-        <TermsListItem key={index} term={term} />
+        <TermsListItem key={index} term={term} url={urls[index]} />
       ))}
     </Container>
   );
